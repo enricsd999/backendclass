c16cf0225b99ef2e9c9468118cbeb3b0d6397d08
unknown <enricsd999@gmail.com>
2026-03-09 11:12:50 +0800

    Remove Handlers

--- routes/web.go
+++ routes/web.go
@@ -7,8 +7,6 @@ import (
 
 func RegisterRoutes() {
 	http.HandleFunc("POST /login", handlers.Login)
-	http.HandleFunc("POST /update", handlers.Update)
-	http.HandleFunc("POST /data", handlers.ShowData)
 
 	http.ListenAndServe(":8080", nil)
 }
