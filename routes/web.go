84032aedb21b1a29449917c8ea2e762a64b5125b
unknown <enricsd999@gmail.com>
2026-03-09 10:25:48 +0800

    Empty

--- routes/web.go
+++ routes/web.go
@@ -1,13 +1,12 @@
 package routes
 
 import (
-	"loginsystem/handlers"
 	"net/http"
 )
 
 func RegisterRoutes() {
-	http.HandleFunc("POST /update", handlers.UpdateData)
-	http.HandleFunc("POST /insert", handlers.InsertData)
+	// http.HandleFunc("POST /update", handlers.UpdateData)
+	// http.HandleFunc("POST /insert", handlers.InsertData)
 
 	http.ListenAndServe(":8080", nil)
 }
