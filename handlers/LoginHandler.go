68f702dc69cc8147dd5821b18511473743ef2697
enricsd999@gmail.com <enricsd999@gmail.com>
2026-03-10 21:59:30 +0800

    Removed DB Config folder

--- handlers/LoginHandler.go
+++ handlers/LoginHandler.go
@@ -1,20 +1,21 @@
 package handlers
 
 import (
+	"database/sql"
 	"fmt"
-	"loginsystem/database"
 	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	//"github.com/golang-jwt/jwt/v5"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
-
-	db, err := database.ConnectDatabase()
+	db, err := sql.Open("mysql", os.Getenv("DSN"))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	_ = db
+
 }
