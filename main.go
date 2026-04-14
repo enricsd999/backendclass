9ea4bbc9dd607ddbd7881f89a2cf23d35cd7c825
enricsd999@gmail.com <enricsd999@gmail.com>
2026-04-14 22:43:38 +0800

    first

--- main.go
+++ main.go
@@ -2,21 +2,34 @@ package main
 
 import (
 	"fmt"
-	"loginsystem/routes"
-
-	"github.com/joho/godotenv"
+	"time"
 )
 
-// @title Login API
-// @version 1.0
-// @description This is a description
-// @securityDefinitions.apikey BearerAuth
-// @in header
-// @name Authorization
+func sayHello(ch chan string) {
+	fmt.Println(<-ch)
+}
+
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println(err)
+
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	// go sayHello(channel)
+	go sayHello2(channel1, channel2)
+	channel1 <- "Goroutine 1"
+	fmt.Println("Main")
+	// time.Sleep(2 * time.Second)
+	channel2 <- "Goroutine 2"
+	// time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second)
+}
+
+func sayHello2(ch1 chan string, ch2 chan string) {
+	for {
+		select {
+		case msg1 := <-ch1:
+			fmt.Println(msg1)
+		case msg2 := <-ch2:
+			fmt.Println(msg2)
+		}
 	}
-	routes.RegisterRoutes()
 }
