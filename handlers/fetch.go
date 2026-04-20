6db20776f1b3f84201e3efc5d49228d4407e7b63
enricsd999@gmail.com <enricsd999@gmail.com>
2026-04-20 17:14:28 +0800

    1st

---
+++ handlers/fetch.go
@@ -0,0 +1,130 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+type Posts struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+}
+type PostResult struct {
+	Posts []Posts `json:"data"`
+	Error error   `json:"error"`
+}
+
+type Users struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+type UserResult struct {
+	Users []Users `json:"data"`
+	Error error   `json:"error"`
+}
+
+type CombinedResult struct {
+	UserResult UserResult `json:"users"`
+	PostResult PostResult `json:"posts"`
+}
+
+// https://jsonplaceholder.typicode.com/posts?_start=0&_limit=3
+func fetchPosts(wg *sync.WaitGroup, jwtToken string, ch chan<- PostResult) {
+	defer wg.Done()
+	// resp, err := http.Get("https://jsonplaceholder.typicode.com/posts?_start=0&_limit=3")
+	req, err := http.NewRequest("GET", "https://jsonplaceholder.typicode.com/posts?_start=0&_limit=3", nil)
+	if err != nil {
+		fmt.Println(err)
+		postRes := PostResult{
+			Posts: nil,
+			Error: err,
+		}
+		ch <- postRes
+		return
+	}
+	req.Header.Set("Authorization", "Bearer "+jwtToken)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		postRes := PostResult{
+			Posts: nil,
+			Error: err,
+		}
+		ch <- postRes
+		return
+	}
+	var posts []Posts
+	json.NewDecoder(resp.Body).Decode(&posts)
+	postRes := PostResult{
+		Posts: posts,
+		Error: nil,
+	}
+	ch <- postRes
+
+}
+
+// https://jsonplaceholder.typicode.com/users?_start=0&_limit=3
+func fetchUsers(wg *sync.WaitGroup, jwtToken string, ch chan<- UserResult) {
+	defer wg.Done()
+	// resp, err := http.Get("https://jsonplaceholder.typicode.com/users?_start=0&_limit=3")
+	req, err := http.NewRequest("GET", "https://jsonplaceholder.typicode.com/users?_start=0&_limit=3", nil)
+	if err != nil {
+		fmt.Println(err)
+		userRes := UserResult{
+			Users: nil,
+			Error: err,
+		}
+		ch <- userRes
+		return
+	}
+	req.Header.Set("Authorization", "Bearer "+jwtToken)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		userRes := UserResult{
+			Users: nil,
+			Error: err,
+		}
+		ch <- userRes
+		return
+	}
+	var users []Users
+	json.NewDecoder(resp.Body).Decode(&users)
+	userRes := UserResult{
+		Users: users,
+		Error: nil,
+	}
+	ch <- userRes
+
+}
+
+func FetchData(w http.ResponseWriter, r *http.Request) {
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	chPosts := make(chan PostResult)
+	chUsers := make(chan UserResult)
+	jwtToken := "Token JWT"
+
+	go fetchPosts(&wg, jwtToken, chPosts)
+	go fetchUsers(&wg, jwtToken, chUsers)
+
+	results := CombinedResult{
+		UserResult: <-chUsers,
+		PostResult: <-chPosts,
+	}
+
+	jsonData, err := json.Marshal(results)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	wg.Wait()
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+}
--- main.go
+++ main.go
@@ -1,85 +1,14 @@
 package main
 
 import (
-	"encoding/json"
+	"backendclass/handlers"
 	"fmt"
 	"net/http"
-	"sync"
 )
 
-type Post struct {
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-}
-
-type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-
-type Todo struct {
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-	Done  bool   `json:"completed"`
-}
-
-func fetchPosts(wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
-	if err != nil {
-		fmt.Println("posts error:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	var post Post
-	json.NewDecoder(resp.Body).Decode(&post)
-
-	fmt.Println("Post:", post.Title)
-}
-
-func fetchUser(wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/users/1")
-	if err != nil {
-		fmt.Println("user error:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	var user User
-	json.NewDecoder(resp.Body).Decode(&user)
-
-	fmt.Println("User:", user.Name)
-}
-
-func fetchTodo(wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
-	if err != nil {
-		fmt.Println("todo error:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	var todo Todo
-	json.NewDecoder(resp.Body).Decode(&todo)
-
-	fmt.Println("Todo:", todo.Title, "Done:", todo.Done)
-}
-
 func main() {
-	var wg sync.WaitGroup
+	http.HandleFunc("/data", handlers.FetchData)
+	http.ListenAndServe(":8080", nil)
 
-	wg.Add(3)
-
-	go fetchPosts(&wg)
-	go fetchUser(&wg)
-	go fetchTodo(&wg)
-
-	wg.Wait()
-	fmt.Println("All API calls finished")
+	fmt.Println("Server running at http://localhost:8080")
 }
